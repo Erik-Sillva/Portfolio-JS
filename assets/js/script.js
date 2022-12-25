41c8dccc02a58ba49794fd17ec38964ec41d9101
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-12-25 01:48:43 -0300

    Finalizando portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,52 @@ styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 
+function activeOn(on) {
+    let home = document.querySelector('.home');
+    let about = document.querySelector('.about');
+    let services = document.querySelector('.services');
+    let portfolio = document.querySelector('.portfolio');
+    let contact = document.querySelector('.contact');
+
+    if (on === 'home') {
+        home.classList.add('active')
+
+        about.classList.remove('active');
+        services.classList.remove('active');
+        portfolio.classList.remove('active');
+        contact.classList.remove('active');
+    } else if (on === 'about'){
+        about.classList.add('active')
+
+        home.classList.remove('active');
+        services.classList.remove('active');
+        portfolio.classList.remove('active');
+        contact.classList.remove('active');
+
+    } else if (on === 'services'){
+        services.classList.add('active');
+
+        home.classList.remove('active');
+        about.classList.remove('active');
+        portfolio.classList.remove('active');
+        contact.classList.remove('active');
+    } else if (on === 'portfolio'){
+        portfolio.classList.add('active');
+
+        home.classList.remove('active');
+        about.classList.remove('active');
+        services.classList.remove('active');
+        contact.classList.remove('active');
+    } else if (on === 'contact'){
+        contact.classList.add('active');
+
+        home.classList.remove('active');
+        about.classList.remove('active');
+        services.classList.remove('active');
+        portfolio.classList.remove('active');
+    }
+}
+
 //hide style switcher on scroll
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open")) {
@@ -35,7 +81,7 @@ daynight.addEventListener('click', () => {
     mode.classList.toggle('fa-sun');
     mode.classList.toggle('fa-moon');
     document.body.classList.toggle('dark');
-})
+});
 
 window.addEventListener('load', () => {
     if (document.body.classList.contains('dark')) {
@@ -45,4 +91,13 @@ window.addEventListener('load', () => {
         mode.classList.add('fa-moon');
         mode.classList.remove('fa-sun');
     }
+});
+
+// Text Effect //
+
+let typed = new Typed('.typing', {
+    strings:["Web Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
 });
