559b5e2eba74ca75ab2aa35cc5a0c3f695bef8ec
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-12-25 18:39:36 -0300

    Validation Form

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,8 +96,94 @@ window.addEventListener('load', () => {
 // Text Effect //
 
 let typed = new Typed('.typing', {
-    strings:["Web Developer", "Web Designer"],
+    strings:["Web Developer", "Web Designer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
+
+// Form Validation //
+
+const form = document.querySelector('#form');
+const inputs = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+function setsucces(index) {
+    if (index == 0) {
+        inputs[0].style.border = '2px solid green'
+        spans[0].style.display = 'none'
+    } else if (index == 1){
+        inputs[1].style.border = '2px solid green'
+        spans[1].style.display = 'none'
+    } else if (index == 2){
+        inputs[2].style.border = '2px solid green'
+        spans[2].style.display = 'none'
+    } else if (index == 3){
+        inputs[3].style.border = '2px solid green'
+        spans[3].style.display = 'none'
+    }
+}
+
+function setErro(index) {
+    if (index == 0) {
+        inputs[0].style.borderColor = '#dc3545'
+        spans[0].style.display = 'block'
+    } else if (index == 1){
+        inputs[1].style.borderColor = '#dc3545'
+        spans[1].style.display = 'block'
+    } else if (index == 2){
+        inputs[2].style.borderColor = '#dc3545'
+        spans[2].style.display = 'block'
+    } else if (index == 3){
+        inputs[3].style.borderColor = '#dc3545'
+        spans[3].style.display = 'block'
+    }
+}
+
+function nameValidate() {
+    if (inputs[0].value != '') {
+        setsucces(0)
+    } else {
+        setErro(0)
+    }
+}
+
+function emailValidate() {
+    if (emailRegex.test(inputs[1].value)) {
+        setsucces(1)
+    } else {
+        setErro(1)
+    }
+}
+
+function subjectValidate() {
+    if (inputs[2].value != '') {
+        setsucces(2)
+    } else {
+        setErro(2)
+    }
+}
+
+function messageValidate() {
+    if (inputs[3].value != '') {
+        setsucces(3)
+    } else {
+        setErro(3)
+    }
+}
+
+function submitValidate() {
+    if (inputs[0].value == '') {
+        setErro(0)
+    }
+    if (inputs[1].value == '') {
+        setErro(1)
+    }
+    if (inputs[2].value == '') {
+        setErro(2)
+    }
+    if (inputs[3].value == '') {
+        setErro(3)
+    }
+}
